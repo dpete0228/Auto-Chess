309d54a56366d60219d318d28aff3f9c0a92a0c5
Travis Newbry <tjnewbry@gmail.com>
2026-02-10 11:56:54 -0700

    fixed board positions

--- board-positions.py
+++ board-positions.py
@@ -1,20 +1,20 @@
 print("Position, X inches, Y inches")
 print("Origin, 0, 0")
 
-colors = ["Black","White"]
+colors = [("Black", "-0.875"),("White", "14.875")]
 dec = 0.875
-for color in colors:
+for pair in colors:
     y = 13.125
     for i in range(15):
-        print(f"{color}Jail{i+1},{" " if i+1<10 else ""} -0.875, {" " if y<10 else ""}{y:.3f}")
+        print(f"{pair[0]}Jail{i+1},{" " if i+1<10 else ""} {pair[1]}, {" " if y<10 else ""}{y:.3f}")
         y -= dec
 
 letters = ["a","b","c","d","e","f","g","h"]
 inc = 1.75
-x = -0.875
+x = 0.875
 for i in range(8):
-    x += inc
-    y = -0.875
+    y = 0.875
     for j in range(8):
-        y += inc
         print(f"{letters[i]}{j+1}, {" " if x<10 else ""}{x}, {" " if y<10 else ""}{y}")
+        y += inc
+    x += inc
